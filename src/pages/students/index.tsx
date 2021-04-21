d1617bbcff81f21fd67d4aa77ffd91710265fe27
柿内暢昌 <nob@studio-kakky.com>
2021-04-21 10:57:13 +0900

    studentsページ追加

---
+++ src/pages/students/index.tsx
@@ -0,0 +1,9 @@
+import Layout from '../../components/shared/layout';
+
+export default function Students(): JSX.Element {
+  return (
+    <Layout>
+      <div>this is students page</div>
+    </Layout>
+  );
+}
