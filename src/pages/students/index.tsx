da8cae9f2a85eabed29810d93328042d67ad8ef5
柿内暢昌 <nob@studio-kakky.com>
2021-04-21 11:32:55 +0900

    SSRのfirst step

--- src/pages/students/index.tsx
+++ src/pages/students/index.tsx
@@ -1,9 +1,24 @@
+import { GetServerSideProps } from 'next';
 import Layout from '../../components/shared/layout';
 
-export default function Students(): JSX.Element {
+interface Props {
+  pageName: string;
+}
+
+export default function Students(props: Props): JSX.Element {
   return (
     <Layout>
-      <div>this is students page</div>
+      <div>{props.pageName}</div>
     </Layout>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (): Promise<{ props: Props }> => {
+  const res = {
+    props: {
+      pageName: 'this is SSR',
+    },
+  };
+
+  return Promise.resolve(res);
+};
